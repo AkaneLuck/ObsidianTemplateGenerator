6e4761fa52a0e6c30e351f3f6995c3720d5ded4b
Akane Luck <akaneluck02@gmail.com>
2026-02-05 20:05:12 +0900

    feat: 管理表のドメインモデルを追加
    ・ManagementTable: 管理表全体の構造
    ・ManagementEntry: 各エントリ（識別子、名前、コメント、任意項目）
    ・任意項目は[(String, String)]で柔軟に表現"

--- src/Design.hs
+++ src/Design.hs
@@ -7,3 +7,16 @@ module Design where
 import Document
 
 type DesignDocument = Document String
+
+data ManagementTable = ManagementTable
+  { tableId :: String
+  , tableName :: String
+  , entries :: [ManagementEntry]
+  }
+
+data ManagementEntry = ManagementEntry
+  { entryId :: String
+  , entryName :: String
+  , comment :: String
+  , attributes :: [(String, String)]  -- 任意項目
+  }
